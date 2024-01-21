f7f4ea851159b17fcd7d54475200c73c993eb6df
nhatv <nhatv2@gmail.com>
2024-01-21 18:14:55 -0500

    Start CoreUltra

---
+++ CoreUltra.cs
@@ -0,0 +1,33 @@
+/*
+name: null
+description: null
+tags: null
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/Army/CoreArmyLite.cs
+using Skua.Core.Interfaces;
+
+public class CoreUltra
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+    private CoreFarms Farm = new();
+    private CoreAdvanced Adv => new();
+    private CoreArmyLite Army = new();
+    private static CoreArmyLite sArmy = new();
+
+    public string OptionsStorage = "Ultra";
+    public bool DontPreconfigure = true;
+
+    public void ScriptMain(IScriptInterface bot)
+    {
+        Core.RunCore();
+    }
+
+    // revitalize
+
+
+}
